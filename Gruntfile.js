f8175d4b85f3bdc7abe5be09b0d98cb572ba2b11
S0019505532 <shawyun.oh@nscorp.com>
2019-08-01 02:36:39 +0000

    update Grunt and package file to improve performance

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,30 @@
-module.exports = function(grunt) {
-            "use strict";
-            grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-build");
-            grunt.registerTask("default", [
-                        "clean",
-                        "lint",
-                        "build"
-            ]);
+module.exports = function (grunt) {
+	"use strict";
+	// Project configuration.
+	grunt.initConfig({
+		uglify: {
+			options: {
+				mangle: true,
+				compress: {
+					drop_console: true,
+					dead_code: false,
+					unused: false
+				}
+			},
+			files: {
+				expand: true,
+				cwd: "<%= ref.staging%>",
+				src: ["**/*.js", "!test/**", "!test_local.html"],
+				dest: "<%= ref.process%>"
+			}
+		}
+	});
+
+	grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-build");
+	grunt.registerTask("default", [
+		"clean",
+		"lint",
+		"build",
+		"uglify"
+	]);
 };
